d4f1b918bda4b2c5278c4c4ece5d8ebe5a30de68
ahmedbabiker317 <112027175+ahmedbabiker317@users.noreply.github.com>
2022-08-24 17:21:41 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/****************************************
+* Hello.java
+**********************************
+*Ahmed Abdelrahman Babiker
+*08/24/2022
+*255/001
+*******************************/
+
+public class Hello{
+	public static void main(String[] args) {
+	
+		System.out.println("<Ahmed Abdelrahman Babiker>");
+		System.out.println("<Hello.java>");
+		System.out.println("<print>");
+		System.out.println("<08/24/2022>");
+		System.out.println("<255/001>");
+		System.out.println();
+	}
+}
